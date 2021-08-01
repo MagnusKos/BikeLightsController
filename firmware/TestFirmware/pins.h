3d3e6a738cd19bf554ebb33a3f83d571c6e369f1
Constantine <magnuskos@gmail.com>
2021-08-01 23:53:40 +0300

    Pins editing
    Just a small editing of "pins.h", so it looks a bit more hip.
    Added digital pins for a single button per pin configuration.

--- firmware/TestFirmware/pins.h
+++ firmware/TestFirmware/pins.h
@@ -1,14 +1,25 @@
 #pragma once
 
-//*** Pins definition section ***
+//*** PWM pins ***
 
-#define P_BEAM_LOW 5
-#define P_BEAM_HIGH 3
-#define P_BEAM_REAR 6
+#define P_BEAM_LOW     5
+#define P_BEAM_HIGH    3
+#define P_BEAM_REAR    6
 
-#define P_VOLTAGE A3
-#define P_LUX A1
-#define P_BUTTON A2
+// *** ADC inputs ***
 
-#define P_LED_R 7
-#define P_LED_Y 8
+#define P_VOLTAGE     A3
+#define P_LUX         A1
+#define P_BUTTON_MUL  A2  //multiplexed buttons
+
+// *** Digital buttons ***
+
+#define P_BUTTON_A     8
+#define P_BUTTON_B     9
+#define P_BUTTON_C    10
+
+
+// *** LEDs' pins ***
+
+#define P_LED_R        7
+#define P_LED_Y        8
